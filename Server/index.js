d39d52022c289e16736db65f406eea6880c2ff6b
yashodha1ariyarathne <thamarakumari1980413@gmail.com>
2022-05-30 15:27:08 +0530

    add all routers to this file

--- Server/index.js
+++ Server/index.js
@@ -2,12 +2,22 @@ var express=require("express");
 var bodyParser=require('body-parser');
 var app = express();
 
-var authenticateController=require('./login_route');
+var authenticateControllerlogin=require('/login/login_route');
+var authenticateControllermanage=require('./login/adminlogin_route');
+var adminControlleradd=require('./administation/add_teacher');
+var adminControllerremove=require('./administation/remove_teacher');
+var adminControllerview=require('./administation/view_teacher');
+var attendanceController=require('./attendance/attendance_view');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-app.post('/login_route',authenticateController.authenticate);
+app.post('/login/login_route',authenticateControllerlogin.authenticatelogin);
+app.post('/login/adminlogin_route',authenticateControllermanage.authenticatemanage);
+app.post('/administation/add_teacher',adminControlleradd.addteacher);
+app.post('/administation/remove_teacher',adminControllerremove.removeteacher);
+app.post('/administation/view_teacher',adminControllerview.viewteacher);
+app.post('/attendance/attendance_view',attendanceController.viewattendance);
 
 
 app.listen(8012);
